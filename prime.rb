782e3ce97ab6dcc7d25400efb3ef025b92ee8855
Suchitra Naik <susanaik@gmail.com>
2018-04-26 16:15:36 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -9,20 +9,38 @@ def prime?(number)
   if(number == 1 || number == 0 || number == -1)
     return false 
   end
+<<<<<<< HEAD
+=======
+  
+  Benchmark.bm do |bm|
+    
+>>>>>>> 7df6938166bdfc7b152509009190ef2eab43995a
       i = 2 
       array_integer = []
       
     
+<<<<<<< HEAD
  
+=======
+  bm.reportdo
+>>>>>>> 7df6938166bdfc7b152509009190ef2eab43995a
       while i < number
       array_integer << i
          i += 1 
       end
+<<<<<<< HEAD
 
  
 
   #puts array_integer
  
+=======
+  end
+ 
+
+  #puts array_integer
+  bm.reportdo
+>>>>>>> 7df6938166bdfc7b152509009190ef2eab43995a
     
   array_integer.each do |num_int|
     if number%num_int == 0 && number != num_int
@@ -30,6 +48,11 @@ def prime?(number)
     end
   end
   return true
+<<<<<<< HEAD
 
+=======
+end
+end
+>>>>>>> 7df6938166bdfc7b152509009190ef2eab43995a
   
 end 
